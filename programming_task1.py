b0ed086a6fd8a4da8f76ca61d18dc492cf38c416
asmit ghimire <asmitsmit3@gmail.com>
2023-11-05 22:23:51 +0545

    assignment week1

---
+++ programming_task1.py
@@ -0,0 +1,36 @@
+print("Hello World")
+
+print("Hello, Asmit!")
+
+Celsius=38.4
+Fahrenheit=(Celsius*(9/5))+32
+
+print("temp in Fahrenheit= ", Fahrenheit)
+print("temp in Celsius= ", Celsius)
+
+batted=1014
+Not_out=162
+runs_scored=48426
+innings= batted-Not_out
+batting_average= runs_scored/innings
+print("Boycott's batting average=", batting_average)
+
+lab_group=24
+student1=113
+student2=175
+student3=12
+
+group1=(student1//lab_group)
+group2=(student2//lab_group)
+group3=(student3//lab_group)
+
+leftovers1=(student1%lab_group)
+leftovers2=(student2%lab_group)
+leftovers3=(student3%lab_group)
+
+print(group1)
+print(group2)
+print(group3)
+print("left overs1=", leftovers1)
+print("left overs2=", leftovers2)
+print("left overs3=", leftovers3)
